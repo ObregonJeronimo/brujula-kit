f1f7a03d8e0e26282fcf3cc00b53dc7d69102221
ThiagoJoelP <thiagojoel17@hotmail.com>
2026-06-16 18:47:24 -0300

    feat: mount Root component instead of App for landing/app routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./evalkit.jsx";
+import Root from "./Root.jsx";
 import "./styles/theme.css";
 import "./styles/global.css";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
